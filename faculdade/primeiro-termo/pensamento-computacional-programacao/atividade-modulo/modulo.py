52b42185dde1887ec2961d1e6cc78b8843e35970
André Luis <andreluis1wa@gmail.com>
2025-12-10 10:51:58 -0300

    Create modulo.py

---
+++ faculdade/primeiro-termo/pensamento-computacional-programacao/atividade-modulo/modulo.py
@@ -0,0 +1,20 @@
+def somar(a,b):
+    return a+b 
+
+
+def verificar(a):
+    if a > 0:
+        print("É um número positivo!")
+    elif a < 0:
+        print("É um número negativo!")
+    else:
+        print("É o número zero!!!")
+
+
+
+def idade(a):
+    if a > 18:
+        print("Você é de maior!")
+    else:
+        print("Você é de menor!!")
+        
